4aa86d516064415ae76eff3e588b7e153988a149
Ravi Bharti <ravics1718@gmail.com>
2021-09-29 02:00:03 +0530

    🍔 add auth, post actions

---
+++ src/store/actions/auth.js
@@ -0,0 +1,65 @@
+import { login, signup } from "./index"; //actions
+import { auth } from "../../apis"; //auth api
+
+export const loginToGithub = (payload) => {
+  return (dispatch) => {
+    dispatch(loginToGithubStart());
+    auth
+      .login(payload)
+      .then((response) => {
+        loginToGithubSuccess(response.data);
+      })
+      .catch((err) => {
+        loginToGithubFailed(err);
+      });
+  };
+};
+const loginToGithubStart = () => {
+  return {
+    type: login.LOGIN_START,
+  };
+};
+const loginToGithubSuccess = (data) => {
+  return {
+    type: login.LOGIN_SUCEESS,
+    data: data,
+  };
+};
+const loginToGithubFailed = (err) => {
+  return {
+    type: login.LOGIN_FAILD,
+    data: err,
+  };
+};
+
+//signup
+export const signupToGithub = (payload) => {
+  return (dispatch) => {
+    dispatch(signupToGithubStart());
+    auth
+      .signup(payload)
+      .then((response) => {
+        signupToGithubSuccess(response.data);
+      })
+      .catch((err) => {
+        signupToGithubFailed(err);
+      });
+  };
+};
+const signupToGithubStart = () => {
+  return {
+    type: signup.SIGNUP_START,
+  };
+};
+const signupToGithubSuccess = (data) => {
+  return {
+    type: signup.SIGNUP_SUCEESS,
+    data: data,
+  };
+};
+const signupToGithubFailed = (err) => {
+  return {
+    type: signup.CLEAR_SIGNUP,
+    data: err,
+  };
+};
---
+++ src/store/actions/post.js
@@ -0,0 +1,34 @@
+import { createPost } from "./index";
+import { post } from "../../apis";
+
+export const createNewPost = (payload) => {
+  return (dispatch) => {
+    dispatch(createPostStart());
+    post
+      .create(payload)
+      .then((response) => {
+        dispatch(createPostSuccess(response.data));
+      })
+      .catch((err) => {
+        dispatch(createPostFaild(err));
+      });
+  };
+};
+
+const createPostStart = () => {
+  return {
+    type: createPost.POST_CREATE_START,
+  };
+};
+const createPostSuccess = (data) => {
+  return {
+    type: createPost.POST_CREATE_SUCEESS,
+    data: data,
+  };
+};
+const createPostFaild = (err) => {
+  return {
+    type: createPost.POST_CREATE_FAILD,
+    data: err,
+  };
+};
