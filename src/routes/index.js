d260b63dc690e8b60eef1dff3c378dd2a256e2f5
Ravi Bharti <ravics1718@gmail.com>
2021-10-14 00:59:32 +0530

    added profile route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import Login from "../pages/Auth/Login";
 import Signup from "../pages/Auth/SignUp";
 import Feeds from "../pages/feed";
 import $NotFound from "../pages/Error";
+import Profile from "../pages/profile";
 
 const Routes = () => {
   return (
@@ -16,6 +17,7 @@ const Routes = () => {
         <Route exact path='/signin' component={Login} />
         <Route exact path='/signup' component={Signup} />
         <Route exact path='/feed' component={Feeds} />
+        <Route exact path='/profile' component={Profile} />
         <Route path='/*' component={$NotFound} />
       </Switch>
     </BrowserRouter>
