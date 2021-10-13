528154baaa557ed467df4cfc51be1fe714966386
Ravi Bharti <ravics1718@gmail.com>
2021-10-14 00:56:28 +0530

    Rendered dummy posts

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -6,10 +6,13 @@ import Post from "../../Components/Post";
 import CreatePost from "../../Components/Post/CreatePost";
 import CreatePostForm from "../../Components/Post/CreatePostForm";
 import useStyles from "./style/feed.css";
+import { postLists } from "./data";
 
 const Feeds = () => {
   const styles = useStyles();
   const [openModal, setOpenModal] = useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const [posts, setPosts] = useState(postLists);
 
   const handleClick = () => {
     setOpenModal(!openModal);
@@ -30,7 +33,11 @@ const Feeds = () => {
           <Grid className={styles.leftSide} item xs={0} sm={2} lg={3}></Grid>
           <Grid className={styles.middleSide} item xs={12} sm={8} lg={6}>
             <CreatePost onClick={handleClick} />
-            <Post />
+            <div>
+              {posts.map((post, index) => (
+                <Post post={post} key={index} />
+              ))}
+            </div>
           </Grid>
           <Grid className={styles.rightSide} item xs={0} sm={2} lg={3}></Grid>
         </Grid>
