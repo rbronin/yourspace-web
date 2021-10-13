3e8d624349443c898cc506f848c631758a577ed6
Ravi Bharti <ravics1718@gmail.com>
2021-10-14 00:55:36 +0530

    created dummy data for post

---
+++ src/pages/feed/data.js
@@ -0,0 +1,26 @@
+export const postLists = [
+  {
+    id: "12ej321ep",
+    author: "Ravi Bharti",
+    createdAt: "2021-02-12",
+    content: "Hey there I's my first post😀",
+  },
+  {
+    id: "4t3t09u5039",
+    author: "Vipin Kumar",
+    createdAt: "2021-02-12",
+    content: "So what's up",
+  },
+  {
+    id: "bue90ur0",
+    author: "Ravi Bharti",
+    createdAt: "2021-02-12",
+    content: "I am looking some to work with Golang project",
+  },
+  {
+    id: "svhw0ve9",
+    author: "Raj Kumar",
+    createdAt: "2021-02-12",
+    content: "Hello There, What's going on",
+  },
+];
